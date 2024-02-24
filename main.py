47c0e8e922cf1b90127ce29205ab1ffb35d2da45
Pol <pol.alonso@gmail.com>
2024-02-24 19:35:36 +0100

    Rudimentary dungeon floor with Buttons as tiles.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from kivy.app import App    # type: ignore
 from kivy.uix.boxlayout import BoxLayout    # type: ignore
+from kivy.uix.gridlayout import GridLayout    # type: ignore
 #from kivy.uix.scrollview import ScrollView  # type: ignore
 from kivy.uix.label import Label    # type: ignore
 from kivy.uix.button import Button  # type: ignore
@@ -9,16 +10,40 @@ from kivy.uix.button import Button  # type: ignore
 
 #LabelBase.register(name = 'Vollkorn',
                    #fn_regular= 'fonts/Vollkorn-Regular.ttf',
-                   #fn_italic='fonts/Vollkorn-Italic.ttf')
+                   #fn_italic='fonts/Vollkorn-Italic.ttf'
+
+
+class DungeonFloor(GridLayout):
+    
+    def __init__(self, x_axis = 10, y_axis = 10, **kwargs):
+        super().__init__(**kwargs)
+        self.rows = y_axis
+        self.cols = x_axis
+        self.tiles = y_axis * x_axis
+
+        for tile in range(self.tiles):
+
+            tile = Button()
+            self.add_widget(tile)
 
 
 
 class CrapgeonApp(App):
 
+    pass
 
-    def build (self):
 
-        return Label (text='Crapgeon')
+    #def build (self):
+
+        #dungeon = DungeonFloor()
+
+        #layout = BoxLayout()
+        
+        #layout.add_widget(dungeon)
+
+        #return layout
+
+        #return BoxLayout()
 
     
 
